22c57befd97fa1c8c77c7a3837d2cb4d2dab262e
dananaelle <danelbaze1@gmail.com>
2023-06-08 13:44:04 +0200

    first commit

---
+++ tests/DataTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class DataTest extends TestCase
+{
+/**
+* @dataProvider additionProvider
+*/
+public function testAdd(int $a, int $b, int $expected): void
+{
+$this->assertSame($expected, $a + $b);
+}
+public function additionProvider(): array
+{
+return [
+'adding zeros' => [0, 0, 0],
+'zero plus one' => [0, 1, 1],
+'one plus zero' => [1, 0, 1],
+'one plus one' => [1, 1, 3]
+];
+}
+}
